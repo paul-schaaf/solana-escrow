2b15032995c91273d64e457baac3e5c45eac1a78
Paul Schaaf <paulsimonschaaf@gmail.com>
2021-01-14 18:57:18 +0100

    create entrypoint.rs

---
+++ src/entrypoint.rs
@@ -0,0 +1,18 @@
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    msg!(
+        "process_instruction: {}: {} accounts, data={:?}",
+        program_id,
+        accounts.len(),
+        instruction_data
+    );
+    Ok(())
+}
